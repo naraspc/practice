574af7ea28cc36a8225b68e2802236a402d83dc7
naraspc <wodnrtkfka@naver.com>
2024-01-13 16:58:06 +0900

    feat : Restaurant
    레스토랑 CRUD 기능 추가

--- src/main/java/com/challnege/delivery/domain/restaurant/entity/Restaurant.java
+++ src/main/java/com/challnege/delivery/domain/restaurant/entity/Restaurant.java
@@ -1,9 +1,9 @@
 package com.challnege.delivery.domain.restaurant.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.challnege.delivery.domain.menu.entity.Menu;
+import com.challnege.delivery.global.audit.Category;
+import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,4 +14,33 @@ public class Restaurant {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false)
+    private String restaurantName;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    Category category;
+
+    @Column(nullable = false)
+    private String resNumber;
+
+//
+//    @OneToMany(FetchType.LAZY)
+//    @JoinColumn(name = "menu_id")
+//    private Menu Menu;
+
+    @Builder
+    public Restaurant(long id, String restaurantName, String address, Category category, String resNumber) {
+        this.id = id;
+        this.restaurantName = restaurantName;
+        this.address = address;
+        this.category = category;
+        this.resNumber = resNumber;
+    }
+
+
 }
--- src/main/java/com/challnege/delivery/domain/restaurant/service/RestaurantService.java
+++ src/main/java/com/challnege/delivery/domain/restaurant/service/RestaurantService.java
@@ -1,9 +1,62 @@
 package com.challnege.delivery.domain.restaurant.service;
 
+import com.challnege.delivery.domain.restaurant.dto.RestaurantRequestDto;
+import com.challnege.delivery.domain.restaurant.dto.RestaurantResponseDto;
+import com.challnege.delivery.domain.restaurant.entity.Restaurant;
+import com.challnege.delivery.domain.restaurant.repository.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class RestaurantService {
+
+    private final RestaurantRepository restaurantRepository;
+
+    //Create
+    public RestaurantResponseDto createRestaurant(RestaurantRequestDto restaurantRequestDto) {
+        Restaurant restaurant = Restaurant.builder()
+                .restaurantName(restaurantRequestDto.getRestaurantName())
+                .category(restaurantRequestDto.getCategory())
+                .address(restaurantRequestDto.getAddress())
+                .resNumber(restaurantRequestDto.getResNumber())
+                .build();
+
+        restaurantRepository.save(restaurant);
+
+        return RestaurantResponseDto.fromRestaurantEntity(restaurant);
+    }
+
+
+    //Read find by one
+    public RestaurantResponseDto findRestaurantById(Long id) {
+        Restaurant restaurant = restaurantRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("cannot found restaurant"));
+
+        return RestaurantResponseDto.fromRestaurantEntity(restaurant);
+    }
+
+
+    //Read find all
+    public List<RestaurantResponseDto> findRestaurantByAll() {
+        List<Restaurant> restaurants = restaurantRepository.findAll();
+
+        return RestaurantResponseDto.fromListRestaurantEntity(restaurants);
+
+    }
+
+
+    //Delete
+    public Boolean deleteRestaurant(long id) {
+        Optional<Restaurant> restaurant = restaurantRepository.findById(id);
+
+        if (restaurant.isPresent()) {
+            restaurantRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
