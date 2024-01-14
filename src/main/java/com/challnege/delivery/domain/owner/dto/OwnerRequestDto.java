d59a109b6dd6a8f4d8bea2d554dc450936caa552
DoKkangs <snnam0421@naver.com>
2024-01-15 00:07:29 +0900

    Fix : Setter 추가

--- src/main/java/com/challnege/delivery/domain/owner/dto/OwnerRequestDto.java
+++ src/main/java/com/challnege/delivery/domain/owner/dto/OwnerRequestDto.java
@@ -7,8 +7,10 @@ import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class OwnerRequestDto {
 
