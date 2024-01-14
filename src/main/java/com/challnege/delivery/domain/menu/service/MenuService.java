0aa9742d94e1bdf3ff22c62e8c3ef0296a059a97
SeungbaeLee <aha224610@gmail.com>
2024-01-15 00:30:30 +0900

    merge : menu merge

--- src/main/java/com/challnege/delivery/domain/menu/service/MenuService.java
+++ src/main/java/com/challnege/delivery/domain/menu/service/MenuService.java
@@ -1,6 +1,7 @@
 package com.challnege.delivery.domain.menu.service;
 
 
+import com.challnege.delivery.domain.menu.dto.MenuImageResponseDto;
 import com.challnege.delivery.domain.menu.dto.MenuRequestDto;
 import com.challnege.delivery.domain.menu.dto.MenuResponseDto;
 import com.challnege.delivery.domain.menu.entity.Menu;
@@ -10,9 +11,7 @@ import com.challnege.delivery.domain.restaurant.repository.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -25,16 +24,11 @@ public class MenuService {
     
     // 메뉴 등록
     @Transactional
-    public MenuResponseDto createMenu(Long restaurantsId, /*MultipartFile image,*/ MenuRequestDto menuRequestDto) {
+    public MenuResponseDto createMenu(Long restaurantsId, String image, MenuRequestDto menuRequestDto) {
         Restaurant restaurant = restaurantRepository.findById(restaurantsId).orElseThrow(
                 () -> new NoSuchElementException("음식점을 찾을 수 없습니다."));
-//        byte[] imageBytes;
-//        try {
-//            imageBytes = image.getBytes();
-//        } catch (IOException e) {
-//            throw new RuntimeException("이미지 파일을 찾을 수 없습니다.", e);
-//        }
-        Menu menu = new Menu(restaurant, /*imageBytes,*/ menuRequestDto);
+
+        Menu menu = new Menu(restaurant, image, menuRequestDto);
         Menu createMenu = menuRepository.save(menu);
         return new MenuResponseDto(createMenu);
     }
@@ -50,20 +44,28 @@ public class MenuService {
 
     // 메뉴 수정
     @Transactional
-    public MenuResponseDto updateMenu(Long restaurantsId, Long menuId, /*MultipartFile image,*/ MenuRequestDto menuRequestDto) {
+    public MenuResponseDto updateMenu(Long restaurantsId, Long menuId, MenuRequestDto menuRequestDto) {
         Restaurant restaurant = restaurantRepository.findById(restaurantsId).orElseThrow(
                 () -> new NoSuchElementException("음식점을 찾을 수 없습니다."));
         Menu menu = menuRepository.findById(menuId).orElseThrow(
                 () -> new NoSuchElementException("메뉴를 찾을 수 없습니다."));
-//        byte[] imageBytes;
-//        try {
-//            imageBytes = image.getBytes();
-//        } catch (IOException e) {
-//            throw new RuntimeException("이미지 파일을 찾을 수 없습니다.", e);
-//        }
-        menu.updateMenu(restaurant, /*imageBytes,*/ menuRequestDto);
+
+        menu.updateMenu(restaurant, menuRequestDto);
 
         return new MenuResponseDto(menu); // 빌더패턴? -> 등록일말고 수정일만 넘기게
+    }
+
+    // 메뉴 이미지 수정
+    @Transactional
+    public MenuImageResponseDto updateMenuImage(Long restaurantsId, Long menuId, String imageUrl) {
+        Restaurant restaurant = restaurantRepository.findById(restaurantsId).orElseThrow(
+                () -> new NoSuchElementException("음식점을 찾을 수 없습니다."));
+        Menu menu = menuRepository.findById(menuId).orElseThrow(
+                () -> new NoSuchElementException("메뉴를 찾을 수 없습니다."));
+
+        menu.updateMenuImage(restaurant, imageUrl);
+
+        return new MenuImageResponseDto(menu);
     }
 
     // 메뉴 삭제
