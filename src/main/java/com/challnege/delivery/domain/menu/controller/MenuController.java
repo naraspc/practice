50a4c2d0081a2706f25e60120d2746d3c9a26f8d
hyun <kingmandoo95@gmail.com>
2024-01-14 21:00:35 +0900

    feat : 이미지 C,U / RestController, Controller 분리

--- src/main/java/com/challnege/delivery/domain/menu/controller/MenuController.java
+++ src/main/java/com/challnege/delivery/domain/menu/controller/MenuController.java
@@ -1,58 +1,77 @@
 package com.challnege.delivery.domain.menu.controller;
 
 
+import com.challnege.delivery.domain.menu.dto.MenuImageResponseDto;
 import com.challnege.delivery.domain.menu.dto.MenuRequestDto;
 import com.challnege.delivery.domain.menu.dto.MenuResponseDto;
+import com.challnege.delivery.domain.menu.service.ImageS3Service;
 import com.challnege.delivery.domain.menu.service.MenuService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.io.IOException;
 
 @Slf4j
-@RestController
+@Controller
 @RequestMapping("/restaurant")
 @RequiredArgsConstructor
 public class MenuController {
 
     private final MenuService menuService;
+    private final ImageS3Service imageS3Service;
 
     // 메뉴 등록
     @PostMapping("/{restaurantsId}/menus")
-    public ResponseEntity<MenuResponseDto> createMenu(
+    public String createMenu(
             @PathVariable Long restaurantsId,
-//            @RequestPart MultipartFile image,
-            @RequestPart MenuRequestDto menuRequestDto) {
-        MenuResponseDto menuResponseDto = menuService.createMenu(restaurantsId, /*image,*/ menuRequestDto);
-        return ResponseEntity.ok(menuResponseDto);
+            @RequestPart MultipartFile image,
+            @ModelAttribute MenuRequestDto menuRequestDto,
+            Model model) throws IOException {
+        String imageUrl = imageS3Service.saveFile(image);
+        MenuResponseDto menuResponseDto = menuService.createMenu(restaurantsId, imageUrl, menuRequestDto);
+        model.addAttribute("menuResponse", menuResponseDto);
+        return "menu";
     }
 
-    // 메뉴 전체 조회
-    @GetMapping("/{restaurantsId}/menus")
-    public ResponseEntity<List<MenuResponseDto>> readAllMenu(@PathVariable Long restaurantsId) {
-        List<MenuResponseDto> menuResponseDtoList = menuService.readAllMenu(restaurantsId);
-        return ResponseEntity.ok(menuResponseDtoList);
-    }
+//    // 메뉴 조회 (검색한다면?)
+//    @GetMapping("/{restaurantsId}/menus")
+//    public String readAllMenu(@PathVariable Long restaurantsId) {
+//
+//    }
 
-    // 메뉴 수정
+    // 메뉴 수정 (메뉴 이름, 가격)
     @PutMapping("/{restaurantsId}/menus/{menuId}")
-    public ResponseEntity<MenuResponseDto> updateMenu(
+    public String updateMenu(
             @PathVariable Long restaurantsId,
             @PathVariable Long menuId,
-//            @RequestPart MultipartFile image,
-            @RequestPart MenuRequestDto menuRequestDto) {
-        MenuResponseDto menuResponseDto = menuService.updateMenu(restaurantsId, menuId, /*image,*/ menuRequestDto);
-        return ResponseEntity.ok(menuResponseDto);
+            @ModelAttribute MenuRequestDto menuRequestDto,
+            Model model) {
+        MenuResponseDto menuResponseDto = menuService.updateMenu(restaurantsId, menuId, menuRequestDto);
+        model.addAttribute("menuResponse", menuResponseDto);
+        return "menu";
+    }
+
+    // 메뉴 사진 수정
+    @PutMapping("/{restaurantsId}/menus/{menuId}/images")
+    public String updateMenuImage(
+            @PathVariable Long restaurantsId,
+            @PathVariable Long menuId,
+            @RequestPart MultipartFile image,
+            Model model) throws IOException {
+        String imageUrl = imageS3Service.saveFile(image);
+        MenuImageResponseDto menuImageResponseDto = menuService.updateMenuImage(restaurantsId, menuId, imageUrl);
+        model.addAttribute("menuImageResponse", menuImageResponseDto);
+        return "menu";
     }
 
     // 메뉴 삭제 (Delete는 서비스에서 안정성 문제가 있어서 고려해봐야함)
     @DeleteMapping("/{restaurantsId}/menus/{menuId}")
-    public ResponseEntity<String> deleteMenu(@PathVariable Long restaurantsId, @PathVariable Long menuId) {
+    public String deleteMenu(@PathVariable Long restaurantsId, @PathVariable Long menuId) {
         menuService.deleteMenu(restaurantsId, menuId);
-        return new ResponseEntity<>("삭제 완료", HttpStatus.OK);
+        return "menu";
     }
 }
--- src/main/java/com/challnege/delivery/domain/menu/dto/MenuResponseDto.java
+++ src/main/java/com/challnege/delivery/domain/menu/dto/MenuResponseDto.java
@@ -1,13 +1,18 @@
 package com.challnege.delivery.domain.menu.dto;
 
 import com.challnege.delivery.domain.menu.entity.Menu;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Getter
+@NoArgsConstructor
 public class MenuResponseDto {
     private Long id;
     private String foodName;
     private int price;
+    private String imageUrl;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
 
@@ -15,6 +20,7 @@ public class MenuResponseDto {
         this.id = createMenu.getId();
         this.foodName = createMenu.getFoodName();
         this.price = createMenu.getPrice();
+        this.imageUrl = createMenu.getImageUrl();
         this.createdAt = createMenu.getCreatedAt();
         this.modifiedAt = createMenu.getModifiedAt();
     }
